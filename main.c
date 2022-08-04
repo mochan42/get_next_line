e868585571f12d3679a41b714f799dbf8f6114d1
Monine Chan <mochan@students.42wolfsburg.de>
2022-08-04 13:56:36 +0200

    adding main.c and input file my_text to be able to test get next line

---
+++ main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mochan <mochan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/27 12:23:51 by mochan            #+#    #+#             */
+/*   Updated: 2022/06/06 11:35:22 by mochan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("my_text", O_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd);
+		printf("%s", line);
+		if (line == NULL)
+			break ;
+		free(line);
+	}
+	
+
+	return (0);
+}
